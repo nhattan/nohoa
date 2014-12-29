bebcd3051ba2c5e82f81ec2cede5f2658efa0f42
Le Van Nghia <nghialv.bk@gmail.com>
2014-12-29 22:01:50 +0700

    Pweb started

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Mood::Application.load_tasks
+Pweb::Application.load_tasks
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Mood::Application.config.session_store :cookie_store, key: '_mood_session'
+Pweb::Application.config.session_store :cookie_store, key: '_pweb_session'
